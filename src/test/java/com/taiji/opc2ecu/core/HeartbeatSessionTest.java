0d8a478f0892a75ad7f40a5ece30706f643fa469
peakforever <peakforever@local>
2026-08-20 15:21:38 +0800

    Test heartbeat send failure handling

--- src/test/java/com/taiji/opc2ecu/core/HeartbeatSessionTest.java
+++ src/test/java/com/taiji/opc2ecu/core/HeartbeatSessionTest.java
@@ -23,8 +23,9 @@ public class HeartbeatSessionTest {
     @Test public void validResponseRecoversOffline(){final Events e=new Events();final HeartbeatSession s=session(new FakeChannel(),e);for(int i=0;i<3;i++){long id=s.sendHeartbeat();s.onTimeout(id);}long id=s.sendHeartbeat();assertTrue(s.acceptResponse(response(id)));assertTrue(s.isOnline());assertEquals(1,e.recovered);}
     @Test public void offlineSessionKeepsSending(){final FakeChannel c=new FakeChannel();final HeartbeatSession s=session(c,new Events());for(int i=0;i<4;i++){long id=s.sendHeartbeat();s.onTimeout(id);}assertEquals(4,c.sent.size());}
     @Test public void validResponseResetsFailureCount(){final HeartbeatSession s=session(new FakeChannel(),new Events());long a=s.sendHeartbeat();s.onTimeout(a);long b=s.sendHeartbeat();s.acceptResponse(response(b));assertEquals(0,s.getConsecutiveFailures());}
+    @Test public void sendFailureCountsAsTimeout(){final FakeChannel c=new FakeChannel();c.fail=true;final HeartbeatSession s=session(c,new Events());s.sendHeartbeat();assertEquals(1,s.getConsecutiveFailures());}
     private HeartbeatSession session(FakeChannel c,Events e){HeartbeatSession s=new HeartbeatSession(c,e);sessions.add(s);return s;}
     private byte[] response(long id){return ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putInt((int)id).array();}
     static class Events implements HeartbeatSession.StateListener{int offline,recovered;public void onOffline(){offline++;}public void onRecovered(){recovered++;}}
-    static class FakeChannel implements DatagramChannel{final List<byte[]>sent=new ArrayList<byte[]>();public void send(byte[]p){sent.add(p);}public byte[]receive()throws IOException{throw new IOException("none");}public void setReceiveTimeout(int m){}public void close(){}}
+    static class FakeChannel implements DatagramChannel{final List<byte[]>sent=new ArrayList<byte[]>();boolean fail;public void send(byte[]p)throws IOException{if(fail)throw new IOException("send");sent.add(p);}public byte[]receive()throws IOException{throw new IOException("none");}public void setReceiveTimeout(int m){}public void close(){}}
 }
